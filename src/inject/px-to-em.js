ee89671ab8fe15ff1d16241badb1a1c49a68efad
Tiger Oakes <tigeroakes@gmail.com>
2018-08-11 19:23:18 -0400

    Handles inline style attribute

--- src/inject/px-to-em.js
+++ src/inject/px-to-em.js
@@ -2,7 +2,8 @@
 
 /**
  * Iterate through the stylesheets in a document and replace pixel font sizes
- * with em based values.
+ * with em based values. Afterwards, do the same for all elements with inline
+ * styles.
  */
 function pxToEm() {
   for (let i = 0; i < document.styleSheets.length; i++) {
@@ -13,6 +14,13 @@ function pxToEm() {
       if (err.name !== "SecurityError") throw err;
       else console.warn(i, err);
     }
+  }
+
+  /** @type {NodeListOf<HTMLElement>} */
+  const inlineStyles = document.querySelectorAll('[style*="px"]');
+  for (const { style } of inlineStyles) {
+    const newValues = calculateNewSizes(style.fontSize, style.lineHeight);
+    Object.assign(style, newValues);
   }
 }
 
@@ -45,21 +53,40 @@ function processRules(rules) {
  */
 function processStyle(style) {
   if (style.cssText.includes("px")) {
-    const fontSize = style.getPropertyValue("font-size");
-    if (fontSize && fontSize.endsWith("px")) {
-      const fontSizeValue = parseFloat(fontSize);
-      const lineHeight = style.getPropertyValue("line-height");
+    const { fontSize, lineHeight } = calculateNewSizes(
+      style.getPropertyValue("font-size"),
+      style.getPropertyValue("line-height")
+    );
 
-      if (lineHeight && lineHeight.endsWith("px")) {
-        const lineHeightValue = parseFloat(lineHeight);
-        const relativeLineHeight = lineHeightValue / fontSizeValue;
-        style.setProperty("line-height", `${relativeLineHeight}em`);
-      }
-
-      const relativeFontSize = fontSizeValue / 16;
-      style.setProperty("font-size", `${relativeFontSize}em`);
-    }
+    if (fontSize) style.setProperty("font-size", fontSize);
+    if (lineHeight) style.setProperty("line-height", lineHeight);
   }
+}
+
+/**
+ * Calculate new font size to replace pixel sizes with em based sizes.
+ * Returns an object with new values.
+ * @param {string} fontSize
+ * @param {string} lineHeight
+ */
+function calculateNewSizes(fontSize, lineHeight) {
+  /** @type {{ fontSize?: string, lineHeight?: string }} */
+  let result = {};
+
+  if (fontSize && fontSize.endsWith("px")) {
+    const fontSizeValue = parseFloat(fontSize);
+
+    if (lineHeight && lineHeight.endsWith("px")) {
+      const lineHeightValue = parseFloat(lineHeight);
+      const relativeLineHeight = lineHeightValue / fontSizeValue;
+      result.lineHeight = `${relativeLineHeight}em`;
+    }
+
+    const relativeFontSize = fontSizeValue / 16;
+    result.fontSize = `${relativeFontSize}em`;
+  }
+
+  return result;
 }
 
 pxToEm();

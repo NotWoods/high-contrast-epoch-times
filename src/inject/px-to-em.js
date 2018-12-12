4c8957fe3f791ad4db51953e18c306b035bfcecc
Tiger Oakes <tigeroakes@gmail.com>
2018-12-12 14:11:02 -0800

    Add prettier and schema check

--- src/inject/px-to-em.js
+++ src/inject/px-to-em.js
@@ -6,22 +6,23 @@
  * styles.
  */
 function pxToEm() {
-  for (let i = 0; i < document.styleSheets.length; i++) {
-    const styleSheet = /** @type {CSSStyleSheet} */ (document.styleSheets[i]);
-    try {
-      processRules(styleSheet.cssRules);
-    } catch (err) {
-      if (err.name !== "SecurityError") throw err;
-      else console.warn(i, err);
+    for (let i = 0; i < document.styleSheets.length; i++) {
+        const styleSheet =
+            /** @type {CSSStyleSheet} */ (document.styleSheets[i]);
+        try {
+            processRules(styleSheet.cssRules);
+        } catch (err) {
+            if (err.name !== 'SecurityError') throw err;
+            else console.warn(i, err);
+        }
     }
-  }
 
-  /** @type {NodeListOf<HTMLElement>} */
-  const inlineStyles = document.querySelectorAll('[style*="px"]');
-  for (const { style } of inlineStyles) {
-    const newValues = calculateNewSizes(style.fontSize, style.lineHeight);
-    Object.assign(style, newValues);
-  }
+    /** @type {NodeListOf<HTMLElement>} */
+    const inlineStyles = document.querySelectorAll('[style*="px"]');
+    for (const { style } of inlineStyles) {
+        const newValues = calculateNewSizes(style.fontSize, style.lineHeight);
+        Object.assign(style, newValues);
+    }
 }
 
 /**
@@ -30,37 +31,37 @@ function pxToEm() {
  * @param {CSSRuleList} rules
  */
 function processRules(rules) {
-  for (let j = 0; j < rules.length; j++) {
-    const rule = rules[j];
-    switch (rule.type) {
-      case CSSRule.STYLE_RULE:
-        const { style } = /** @type {CSSStyleRule} */ (rule);
-        processStyle(style);
-        break;
-      case CSSRule.MEDIA_RULE:
-        const { cssRules } = /** @type {CSSGroupingRule} */ (rule);
-        processRules(cssRules);
-        break;
+    for (let j = 0; j < rules.length; j++) {
+        const rule = rules[j];
+        switch (rule.type) {
+            case CSSRule.STYLE_RULE:
+                const { style } = /** @type {CSSStyleRule} */ (rule);
+                processStyle(style);
+                break;
+            case CSSRule.MEDIA_RULE:
+                const { cssRules } = /** @type {CSSGroupingRule} */ (rule);
+                processRules(cssRules);
+                break;
+        }
     }
-  }
 }
 
 /**
  * Process a CSS style rule by replacing pixel font sizes with em based sizes.
- * Line heights are also updated to use ems if nessecary.
+ * Line heights are also updated to use ems if necessary.
  * @param {CSSStyleDeclaration} style Object representing the styles from a CSS
  * rule.
  */
 function processStyle(style) {
-  if (style.cssText.includes("px")) {
-    const { fontSize, lineHeight } = calculateNewSizes(
-      style.getPropertyValue("font-size"),
-      style.getPropertyValue("line-height")
-    );
+    if (style.cssText.includes('px')) {
+        const { fontSize, lineHeight } = calculateNewSizes(
+            style.getPropertyValue('font-size'),
+            style.getPropertyValue('line-height'),
+        );
 
-    if (fontSize) style.setProperty("font-size", fontSize);
-    if (lineHeight) style.setProperty("line-height", lineHeight);
-  }
+        if (fontSize) style.setProperty('font-size', fontSize);
+        if (lineHeight) style.setProperty('line-height', lineHeight);
+    }
 }
 
 /**
@@ -70,23 +71,23 @@ function processStyle(style) {
  * @param {string} lineHeight
  */
 function calculateNewSizes(fontSize, lineHeight) {
-  /** @type {{ fontSize?: string, lineHeight?: string }} */
-  let result = {};
+    /** @type {{ fontSize?: string, lineHeight?: string }} */
+    let result = {};
 
-  if (fontSize && fontSize.endsWith("px")) {
-    const fontSizeValue = parseFloat(fontSize);
+    if (fontSize && fontSize.endsWith('px')) {
+        const fontSizeValue = parseFloat(fontSize);
 
-    if (lineHeight && lineHeight.endsWith("px")) {
-      const lineHeightValue = parseFloat(lineHeight);
-      const relativeLineHeight = lineHeightValue / fontSizeValue;
-      result.lineHeight = `${relativeLineHeight}em`;
+        if (lineHeight && lineHeight.endsWith('px')) {
+            const lineHeightValue = parseFloat(lineHeight);
+            const relativeLineHeight = lineHeightValue / fontSizeValue;
+            result.lineHeight = `${relativeLineHeight}em`;
+        }
+
+        const relativeFontSize = fontSizeValue / 16;
+        result.fontSize = `${relativeFontSize}em`;
     }
 
-    const relativeFontSize = fontSizeValue / 16;
-    result.fontSize = `${relativeFontSize}em`;
-  }
-
-  return result;
+    return result;
 }
 
 pxToEm();
